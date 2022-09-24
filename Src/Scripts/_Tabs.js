41635ec29ac178db6e67f9cbbf8e32f706a64e43
Ziggy Rafiq <ziggyrafiq@hotmail.com>
2022-09-24 17:54:17 +0100

    Uploading
    Uploading the ZiggyRafiq.GitHub.io website Project

---
+++ Src/Scripts/_Tabs.js
@@ -0,0 +1,27 @@
+/************************************************************************************************************
+*  COPYRIGHT BY ZIGGY RAFIQ (ZAHEER RAFIQ)
+*  LinkedIn Profile URL Address: https://www.linkedin.com/in/ziggyrafiq/ 
+*
+*  Website  :  	Ziggy Rafiq GitHub Website Project
+*  Date     :  	23/09/2022
+*  Author   :  	Ziggy Rafiq (https://www.ziggyrafiq.com)
+*  Notes    :  	
+*  Reminder :	PLEASE DO NOT CHANGE OR REMOVE AUTHOR NAME.
+*
+************************************************************************************************************/
+
+document.getElementById("defaultOpen").click();
+
+function openTab(evt, tabSectionName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabSectionName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
